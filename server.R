71e1e9039b1ced5a6f0fdccc1028043923c5e8d6
Jeff Walker <walker.jeff.d@gmail.com>
2014-08-02 14:53:09 -0400

    initial commit with working app

---
+++ server.R
@@ -0,0 +1,82 @@
+library(shiny)
+library(ggplot2)
+theme_set(theme_bw())
+
+obs <- read.csv('data.csv')
+
+shinyServer(function(input, output) {
+  freq555 <- function(R_A, R_B, C) {
+    1/(0.7*(R_A + 2*R_B)*C)
+  }
+
+  output$plotFreq <- renderPlot({
+    R_A <- input$R_A*1e3
+    R_B <- input$R_B*1e3
+    C <- input$Cap*1e-6
+
+    R_B_seq <- seq(1, 100)*1e2
+    f_seq <- freq555(R_A, R_B_seq, C)
+
+    f_pnt <- freq555(R_A, R_B, C)
+
+    output$textFreq <- renderText(paste0('f(R_water = ', format(R_B, digits=3), ' ohm) = ', format(f_pnt, digits=3), ' Hz'))
+
+    ggplot(data.frame(R_B=R_B_seq, f=f_seq), aes(R_B, f)) +
+      geom_hline(yint=0, alpha=0) +
+      geom_line() +
+      geom_point(aes(R_B, f), data=data.frame(R_B=R_B, f=f_pnt), size=4, color='red') +
+      ylim(0, 5000) +
+      labs(x="Water Resistance (ohm)", y="555 Timer Frequency (Hz)")
+  },  width=600, height=400)
+
+  output$plotFreq2 <- renderPlot({
+    R_A <- input$R_A_2*1e3 # kilo-ohm -> ohm
+    C <- input$Cap_2*1e-6  # uFarad -> Farad
+    LA <- input$LA_2       # L/A -> 1/cm
+
+    S <- input$S_2 # uS/cm
+    R <- 1/(S*1e-6) * LA # ohm
+
+    S_seq <- seq(1, 2000) # uS/cm
+    R_seq <- 1/(S_seq*1e-6) * LA
+    f_seq <- freq555(R_A, R_seq, C)
+
+    f_pnt <- freq555(R_A, R, C)
+
+    output$textFreq2_1 <- renderText(paste0(
+      'R_water(S_water = ', format(S, digits=3), ' uS/cm) = ', format(R, digits=3), ' ohm', '\n')
+    )
+
+    output$textFreq2_2 <- renderText(paste0(
+      'f(S_water = ', format(S, digits=3), ' uS/cm) = ', format(f_pnt, digits=3), ' Hz')
+    )
+
+    ggplot(data.frame(S=S_seq, f=f_seq), aes(S, f)) +
+      geom_hline(yint=0, alpha=0) +
+      geom_line() +
+      geom_point(aes(S, f), data=data.frame(S=S, f=f_pnt), size=4, color='red') +
+      geom_point(aes(S, f), data=obs, color='steelblue') +
+      ylim(0, 5000) +
+      labs(x="Water Conductivity (uS/cm)", y="555 Timer Frequency (Hz)")
+  }, width=600, height=400)
+
+  output$plotTemp <- renderPlot({
+    alpha <- input$alpha
+    Temp <- input$T
+
+    T_seq <- seq(10, 35)
+    S_ratio <- 1/(1 + alpha*(T_seq-25))
+    S_pnt <-  1/(1 + alpha*(Temp-25))
+
+
+    output$textTemp <- renderText(paste0(
+      'S_25 / S_T (T = ', format(Temp, digits=3), ' degC) = ', format(S_pnt, digits=3))
+    )
+
+    ggplot(data.frame(S=S_ratio, T=T_seq), aes(T, S)) +
+      geom_line() +
+      geom_point(aes(T, S), data=data.frame(S=S_pnt, T=Temp), size=4, color='red') +
+      ylim(0, 4) +
+      labs(x="Water Temperature, T (degC)", y=expression(paste(S[25]/S[T])))
+  })
+})
---
+++ ui.R
@@ -0,0 +1,84 @@
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Riffle-ito 555 Conductivity"),
+  withMathJax(),
+
+#   mainPanel(
+    tabsetPanel(
+      tabPanel("Frequency vs. Resistance",
+              sidebarLayout(
+                sidebarPanel(
+                  numericInput("R_A", "Series Resistance (\\(R_A\\), k\\(\\Omega\\))", min = 0.001, max = 100, value = 3.3),
+                  numericInput("Cap", "Capacitor (\\(C\\), \\(\\mu\\)F)", min = 0.001, max = 1000, value = 0.1),
+                  hr(),
+                  h5('Move Red Point'),
+                  numericInput("R_B", "Water Resistance (\\(R_{water}\\), k\\(\\Omega\\))", min = 0.001, max = 100, value = 2.3)
+                ),
+                mainPanel(
+                  plotOutput("plotFreq"),
+                  verbatimTextOutput("textFreq"),
+                  helpText('$$f = \\frac{1}{0.7*(R_A + 2R_{water})*C}$$'),
+                  helpText('$$ f = \\text{Frequency Measured by 555 Timer }(Hz)$$'),
+                  helpText('$$ R_A = \\text{Circuit Series Resistor }(\\Omega) $$'),
+                  helpText('$$ R_{water} = \\text{Water Resistance }(\\Omega) $$'),
+                  helpText('$$ C = \\text{Circuit Capacitor }(F)$$'),
+                  p('Reference: ', a(href='http://publiclab.org/notes/donblair/05-28-2014/riffle-dev-fun-w-five-five-fives', "Riffle dev: 'Fun w/ Five-Five-Fives'"))
+                )
+              )
+      ),
+      tabPanel("Frequency vs. Conductivity",
+               sidebarLayout(
+                 sidebarPanel(
+                   numericInput("R_A_2", "Series Resistance (\\(R_A\\), k\\(\\Omega\\))", min = 0.001, max = 100, value = 3.3),
+                   numericInput("Cap_2", "Capacitor (\\(C\\), \\(\\mu\\)F)", min = 0.001, max = 1000, value = 0.1),
+                   numericInput("LA_2", "Length-Area Ratio (\\(l/A\\), 1/cm)", min = 0.00001, max = 10, value = 1),
+                   hr(),
+                   h5('Move Red Point'),
+                   numericInput("S_2", "Water Conductivity (\\(S_{water}\\), \\(\\mu\\)S/cm)", min = 0.1, max = 10000, value = 1000)
+                 ),
+                 mainPanel(
+                   plotOutput("plotFreq2"),
+                   p('Blue points are measurements made by Don Blair. Data available at ', a(href='https://docs.google.com/spreadsheets/d/1VYbCg0TOsdpzL2XVZbKudoSGCnire0_Q3z77r4luqtA/edit', 'Google Sheet')),
+                   verbatimTextOutput("textFreq2_1"),
+                   verbatimTextOutput("textFreq2_2"),
+                   helpText('$$R_{water} = \\frac{1}{S_{water}} \\frac{l}{A}$$'),
+                   helpText('$$f = \\frac{1}{0.7*\\left(R_A + 2R_{water}\\right)*C}$$'),
+                   helpText('$$f = \\frac{1}{0.7*\\left(R_A + \\frac{2}{S_{water}} \\left(\\frac{l}{A}\\right)\\right)*C}$$'),
+                   helpText('$$ R_{water} = \\text{Water Resistance }(\\Omega) $$'),
+                   helpText('$$ S_{water} = \\text{Water Conductivity }(\\mu S/cm) $$'),
+                   helpText('$$ f = \\text{Frequency Measured by 555 Timer }(Hz) $$'),
+                   helpText('$$ l = \\text{Distance between Electrodes }(cm) $$'),
+                   helpText('$$ A = \\text{Surface Area of Electrodes }(cm^2) $$'),
+                   helpText('$$ R_A = \\text{Circuit Series Resistor }(\\Omega) $$'),
+                   helpText('$$ C = \\text{Circuit Capacitor }(F) $$'),
+                   p('Reference: ', a(href='http://publiclab.org/notes/donblair/05-28-2014/riffle-dev-fun-w-five-five-fives', "Riffle dev: 'Fun w/ Five-Five-Fives'")),
+
+                   p('Reference: ', a(href='http://en.wikipedia.org/wiki/Electrical_conductivity', "Electrical Conductivity"))
+                 )
+               )
+      ),
+      tabPanel("Temperature Correction",
+               sidebarLayout(
+                 sidebarPanel(
+                   sliderInput("alpha", "Temp Coefficient (\\(\\alpha\\), 1/degC)", min = 0, max = 0.2, value = 0.05, step=0.01),
+                   hr(),
+                   h5('Move Red Point'),
+                   numericInput("T", "Temperature (\\(T_{water}\\), degC)", min = 0, max = 50, value = 25)
+                 ),
+                 mainPanel(
+                   plotOutput("plotTemp"),
+                   verbatimTextOutput("textTemp"),
+                   helpText('$$\\frac{S_{25}}{S_T} = \\frac{1}{1 + \\alpha(T-25)}$$'),
+                   helpText('$$ S_{25} = \\text{Specific Conductivity @ 25 degC }(\\mu S/cm) $$'),
+                   helpText('$$ S_{T} = \\text{Conductivity at Temperature } T ( \\mu S/cm) $$'),
+                   helpText('$$ \\alpha = \\text{Temperature Coefficient }(1/degC)$$'),
+                   helpText('$$ T = \\text{Water Temperature }(degC) $$'),
+                   p('Reference: ', a(href="http://www.analyticexpert.com/2011/03/temperature-compensation-algorithms-for-conductivity/", "Temperature Compensation Algorithms for Conductivity"))
+                 )
+               )
+      )
+    )
+))
